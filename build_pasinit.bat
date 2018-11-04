ac4e6e50a5fe17430ea1cb38ea8bcfaad37f6dda
Olin Lathrop <OlinLathrop@users.noreply.github.com>
2018-11-04 16:00:24 -0500

    Move some COGSERVE components back to FILE library.

--- build_pasinit.bat
+++ build_pasinit.bat
@@ -4,7 +4,6 @@ rem   Set up for building a Pascal module.
 rem
 call build_vars
 
-call src_get %srcdir% cogserve.ins.pas
 call src_get %srcdir% cogserve2.ins.pas
 
 call src_getfrom sys base.ins.pas
@@ -13,6 +12,7 @@ call src_getfrom sys sys_sys2.ins.pas
 call src_getfrom util util.ins.pas
 call src_getfrom string string.ins.pas
 call src_getfrom file file.ins.pas
+call src_getfrom file cogserve.ins.pas
 call src_getfrom math math.ins.pas
 call src_getfrom stuff stuff.ins.pas
 
