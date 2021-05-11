ae4ec71efe843e23ca9095dab9c98353eeb4e347
Olin Lathrop <OlinLathrop@users.noreply.github.com>
2021-05-11 12:37:32 -0400

    Added RECENTF script.

--- build_escr.bat
+++ build_escr.bat
@@ -12,6 +12,7 @@ call src_escr make_build
 call src_escr make_pic_project
 call src_escr pic_module_template
 call src_escr pullall
+call src_escr recentf
 call src_escr unseq
 
 call src_escr repo.ins
