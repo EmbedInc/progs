69e273ec0972ad1b0cc8c083afb39bcf65161526
Olin Lathrop <OlinLathrop@users.noreply.github.com>
2024-06-01 11:18:43 -0400

    Updated to builds without circular dependencies.

--- build_pasinit.bat
+++ build_pasinit.bat
@@ -7,12 +7,8 @@ call build_vars
 call treename_var (cog)src/%srcdir% tnam
 cd /d "%tnam%"
 
-call src_getfrom sys base.ins.pas
-call src_getfrom sys sys.ins.pas
+call src_getbase
 call src_getfrom sys sys_sys2.ins.pas
-call src_getfrom util util.ins.pas
-call src_getfrom string string.ins.pas
-call src_getfrom file file.ins.pas
 call src_getfrom file cogserve.ins.pas
 call src_getfrom math math.ins.pas
 call src_getfrom stuff stuff.ins.pas
--- gitget_embed.bat
+++ gitget_embed.bat
@@ -61,7 +61,6 @@ call :gitget buildscr
 call :gitget can
 call :gitget chess
 call :gitget code
-call :gitget csvana
 call :gitget displ
 call :gitget dspic
 call :gitget eagle
